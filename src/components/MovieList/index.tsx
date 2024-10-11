9e87d88c838ba7f8f8ae56dc1db82133022b7c10
dev-vithor <vitinhobrt0@gmail.com>
2024-10-11 17:31:41 -0300

    Add: adicionado o componente do card movie, ja tipado e selecionado as informacoes

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -1,12 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, use } from 'react';
 import './index.scss';
 import axios from 'axios';
 
+export interface MovieType {
+    id: number
+    title: string,
+    poster_path: string,
+    overview: string,
+    vote_average: number,
+}
+
 export default function MovieList() {
-    const [movies, setMovie] = useState([]);
+    const [movies, setMovie] = useState<MovieType[]>([]);
+    useEffect(() => {
+        getMovies()
+    }, [])
     
     const getMovies = () => {
         axios({
@@ -16,15 +28,30 @@ export default function MovieList() {
                 api_key: 'a96bbb6475f9f762c0f26e358898209d',
                 language: 'pt-BR'
             }
-        }).then(response => console.log(response))
-        .catch(err => console.error(err));
-
-        getMovies();
-
+        }).then(response => {
+            setMovie(response.data.results)
+            console.log(response.data.results);
+              
+        }) 
     }
+
     return (
         <ul className="movie-list">
-            <li></li>
+            {movies.map((movie) => 
+                <li key={movie.id} className='movie-card'>
+                    <p>
+                        {movie.title}
+                    </p>
+                    <p className='description'>
+                        {movie.overview}
+                    </p>
+                    <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="img movie list" />
+                    <p>
+                        {movie.vote_average}
+                    </p>
+                </li>
+            
+            )}
         </ul>
     )
 }
